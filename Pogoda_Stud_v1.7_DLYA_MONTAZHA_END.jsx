9ca140f19484a57e1d825eec8792f667281cfca3
Mikhail <sjfh@yandex.ru>
2017-05-21 15:25:14 +0500

    added Copyright

--- Pogoda_Stud_v1.7_DLYA_MONTAZHA_END.jsx
+++ Pogoda_Stud_v1.7_DLYA_MONTAZHA_END.jsx
@@ -1,6 +1,19 @@
-﻿//СКРИПТ ДЛЯ МОНТАЖА студийной погоды!!111!!1
+﻿//~ Copyright 2017-2017 Mikhail Sorokin
+//~  Original sources are available at 
+
+//~  Licensed under the Apache License, Version 2.0 (the "License");
+//~  you may not use this file except in compliance with the License.
+//~  You may obtain a copy of the License at
+
+//~  http://www.apache.org/licenses/LICENSE-2.0
+
+//~  Unless required by applicable law or agreed to in writing, software
+//~  distributed under the License is distributed on an "AS IS" BASIS,
+//~  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//~  See the License for the specific language governing permissions and
+//~  limitations under the License.
+//СКРИПТ ДЛЯ МОНТАЖА студийной погоды!!111!!1
 //temp = mask("Mask 1").maskPath;
-//Author  - Mikhail Sorokin
 
 var myProj = app.project;
 var ControllLayer = searchCompByName("POGODA_DATA").layer("CONTROLL");
